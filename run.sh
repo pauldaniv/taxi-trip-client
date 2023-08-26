435ea40a762cdec0e3f1c59c9e52ac9a17fb999a
Pavlo <paulsegfault@gmail.com>
2023-08-26 23:18:48 +0300

    Added helper script

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+COMMAND=$1
+
+#todo: delegate options after script name to target command
+docker run -e FACADE_API_HOST=http://api.yellow-taxi.me -e EVENT_FILE_LOCAL=false -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v $PWD/token.txt:/workspace/token.txt -it --rm me.local/yt-client login --username pavlo.daniv@sombrainc.com
+
+docker run -e FACADE_API_HOST=http://api.yellow-taxi.me -e EVENT_FILE_LOCAL=false -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v $PWD/token.txt:/workspace/token.txt -it --rm me.local/yt-client event --count 300 --concurrency 30
+
+docker run -e FACADE_API_HOST=http://api.yellow-taxi.me -e EVENT_FILE_LOCAL=false -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v $PWD/token.txt:/workspace/token.txt -it --rm me.local/yt-client totals --year 2018 --month 3 --day 24
+
