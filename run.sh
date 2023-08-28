917eb2709618514e4a102d76a5ebff623cf2568e
Pavlo <paulsegfault@gmail.com>
2023-08-29 00:07:35 +0300

    Remove strict console output

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 COMMAND=$1
+
 docker run -e FACADE_API_HOST=http://api.yellow-taxi.me -e EVENT_FILE_LOCAL=false -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v $PWD/token.txt:/workspace/token.txt -it --rm me.local/yt-client $@ | grep 'c.p.p.y.c'
