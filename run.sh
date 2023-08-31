cef256722366646c3a40302864ce4a6eba9ebec2
Pavlo <paulsegfault@gmail.com>
2023-09-01 00:04:00 +0300

    Update docker image URL

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 COMMAND=$1
 
-docker run -e FACADE_API_HOST=http://api.yellow-taxi.me -e EVENT_FILE_LOCAL=false -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v $PWD/token.txt:/workspace/token.txt -it --rm me.local/yt-client $@ | grep 'c.p.p.y.c'
+docker run -e FACADE_API_HOST=http://api.yellow-taxi.me -e EVENT_FILE_LOCAL=false -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -v $PWD/token.txt:/workspace/token.txt -it --rm $AWS_DOMAIN_OWNER_ID.dkr.ecr.us-east-2.amazonaws.com/taxi-trip-client:latest $@ | grep 'c.p.p.y.c'
